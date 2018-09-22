1ddae593182f9df70bb6fd0a45a46feee7ba3197
lxx1884896 <1523631706@qq.com>
2018-09-22 17:07:43 +0800

    1

--- array/121. Best Time to Buy and Sell Stock.cpp
+++ array/121. Best Time to Buy and Sell Stock.cpp
@@ -20,4 +20,64 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
 
 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        
+//           int len=prices.size();
+        
+//         if(len==0) return 0;
+        
+//         vector<int> res;
+        
+//         for(int i=0;i<len;i++){
+//             int max=0;
+//             for(int j=i+1;j<len;j++){
+                
+//                 if(max<prices[j]-prices[i]){
+                    
+//                     max=prices[j]-prices[i];
+//                 }
+//             }
+            
+//             res.push_back(max);
+//         }
+        
+//         int len1=res.size();
+//         int max1=0;
+//         for(int i=0;i<len1;i++){
+            
+//             if(max1<res[i]){
+                
+//                 max1=res[i];
+//             }
+//         }
+        
+//         return max1;     //////////////////////////////////自己第一想法，但用时较长，O(n^2)的复杂度,耗时太多
+        
+        
+           int n=prices.size();
+        if (n<2) return 0;
+        int mp=0,t=0;
+        for (int i=1;i<n;i++) {
+            //buy at a start point and accumulative difference
+            t+=(prices[i]-prices[i-1]);
+            //until buying at this start point start yielding negative gain, but at next point
+            if (t<0) 
+                t=0;
+            mp=max(mp,t);
+        }
+        return mp;
+        
+        
+        
+        
+        
+    }
+};
+
+
+
+
 
